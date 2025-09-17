96bc483ed0396de5cdb9e430fb514cee8c853994
Anshuman Jadiya <anshuman.jadiya03@gmail.com>
2025-09-17 17:13:09 +0530

    Clean up ConsoleUtils.java by removing blank lines

--- src/main/java/com/githubassistant/utils/ConsoleUtils.java
+++ src/main/java/com/githubassistant/utils/ConsoleUtils.java
@@ -3,9 +3,7 @@ package com.githubassistant.utils;
 import java.util.Scanner;
 
 public class ConsoleUtils {
-
     private static final Scanner scanner = new Scanner(System.in);
-
     public static String prompt(String message) {
         System.out.print(message + ": ");
         return scanner.nextLine().trim();
